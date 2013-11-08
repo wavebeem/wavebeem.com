09a745f9b2826d7a58456bdf26256e922ac68486
Brian Mock <mock.brian@gmail.com>
2013-11-08 00:42:14 -0800

    blah

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
                             'style.css',
                             'index.html',
                             'favicon.ico',
-                            'img/'
+                            'img/*'
                         ],
                         dest: 'dist/'
                     }
