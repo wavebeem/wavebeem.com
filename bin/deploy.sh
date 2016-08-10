45b7990cf15e694073329466b7ddb71c724bab5b
Brian Mock <brian@mockbrian.com>
2016-08-09 20:22:23 -0700

    Updates favicon

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# host=s3://dev.mockbrian.com
-host=s3://mockbrian.com
+host=s3://dev.mockbrian.com
+# host=s3://mockbrian.com
 
 bundle exec jekyll build
-
+convert favicon-16.png favicon.ico
 s3cmd sync \
     --no-mime-magic \
     --acl-public \
