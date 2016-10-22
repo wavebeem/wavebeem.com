dffcf1896d2f14f33ff18c36a6cf398d8a3feae3
Brian Mock <brian@mockbrian.com>
2016-10-22 12:05:07 -0700

    Build script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,15 +5,15 @@ cf_distro="E1ENR65WLK5LCW"
 dev="s3://dev.mockbrian.com"
 prod="s3://mockbrian.com"
 
-bundle exec jekyll build
 convert favicon-16.png favicon-32.png favicon.ico
 
-cd _site
 if [[ $1 = "-p" ]]; then
-  aws s3 sync --acl public-read ./ "$prod"
+  bundle exec jekyll build
+  aws s3 sync --acl public-read _site/ "$prod"
   aws cloudfront create-invalidation \
     --distribution-id "$cf_distro" \
     --paths "/*"
 else
-  aws s3 sync --acl public-read ./ "$dev"
+  bundle exec jekyll build --drafts
+  aws s3 sync --acl public-read _site/ "$dev"
 fi
