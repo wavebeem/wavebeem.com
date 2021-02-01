1e0a48adea0941fc7136095b16b32d9752fd4897
Brian Mock <brian@wavebeem.com>
2021-02-01 10:59:02 -0800

    fix layout jank

--- src/_data/art.js
+++ src/_data/art.js
@@ -1,20 +1,25 @@
 const path = require("path");
 const fs = require("fs");
+const getImageColors = require("get-image-colors");
+const getImageSize = require("image-size");
 
-function getArt() {
-  return fs
-    .readdirSync(path.resolve(__dirname, "../static/img/art"))
-    .map((filename) => {
-      const url = `/static/img/art/${filename}`;
-      let name = filename;
-      name = path.basename(name, ".png");
-      name = path.basename(name, ".gif");
-      name = name.replace(/-/g, " ");
-      name = name.replace(/^(\d\d\d\d) (\d\d) (\d\d) (.*)$/, "$4 ($1-$2-$3)");
-      // TODO: Detect width/height so we can avoid layout changes during image
-      // lazy load in the browser.
-      return { url, name };
-    });
+const root = path.resolve(__dirname, "../static/img/art");
+
+async function getArt() {
+  return await Promise.all(fs.readdirSync(root).map(readInfo));
 }
 
-module.exports = getArt();
+async function readInfo(name) {
+  const filename = path.resolve(root, name);
+  const url = `/static/img/art/${name}`;
+  name = path.basename(name, ".png");
+  name = path.basename(name, ".gif");
+  name = name.replace(/-/g, " ");
+  name = name.replace(/^(\d\d\d\d) (\d\d) (\d\d) (.*)$/, "$4 ($1-$2-$3)");
+  const { width, height } = getImageSize(filename);
+  const colors = await getImageColors(filename);
+  const color = colors[0].hex();
+  return { url, name, width, height, color };
+}
+
+module.exports = getArt;
