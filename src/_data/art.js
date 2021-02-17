6ef90528654d22ab8c60e36fd46ebdf48400ab58
Brian Mock <brian@wavebeem.com>
2021-02-17 09:47:16 -0800

    wavebeem

--- src/_data/art.js
+++ src/_data/art.js
@@ -12,14 +12,17 @@ async function getArt() {
 async function readInfo(name) {
   const filename = path.resolve(root, name);
   const url = `/static/img/art/${name}`;
-  name = path.basename(name, ".png");
-  name = path.basename(name, ".gif");
-  name = name.replace(/-/g, " ");
-  name = name.replace(/^(\d\d\d\d) (\d\d) (\d\d) (.*)$/, "$4 ($1-$2-$3)");
+  name = path
+    .basename(name)
+    .replace(/\.(png|gif)$/, "")
+    .replace(/-/g, " ")
+    .replace(/^(\d\d\d\d) (\d\d) (\d\d) (.*)$/, "$4 ($1-$2-$3)");
   const { width, height } = getImageSize(filename);
   const colors = await getImageColors(filename);
   const color = colors[0].hex();
   return { url, name, width, height, color };
 }
 
+// If this ever gets too slow, check out asset caching
+// https://www.11ty.dev/docs/plugins/cache/
 module.exports = getArt;
