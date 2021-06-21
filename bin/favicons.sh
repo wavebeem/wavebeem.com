ec3d9c19f96a67d0c8327a188207cb95c949fe21
Brian Mock <brian@mockbrian.com>
2021-06-20 19:51:13 -0700

    bigger img

--- bin/favicons.sh
+++ bin/favicons.sh
@@ -2,9 +2,17 @@
 set -eu
 
 cd src/static
-for size in 32 180 192 512; do
+for size in 32; do
   convert \
     "favicon-16.png" \
     -scale "${size}x${size}" \
+    -strip \
     "favicon-${size}.png"
+done
+for size in 48 96 180 192 512; do
+  convert \
+    "brian-24.png" \
+    -scale "${size}x${size}" \
+    -strip \
+    "brian-${size}.png"
 done
