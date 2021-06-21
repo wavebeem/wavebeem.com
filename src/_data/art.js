7ca6b162e572de3420a59a0e4a0ad3fbc73288e7
Brian Mock <brian@mockbrian.com>
2021-06-20 19:13:44 -0700

    okie dokie

--- src/_data/art.js
+++ src/_data/art.js
@@ -9,18 +9,20 @@ async function getArt() {
   return await Promise.all(fs.readdirSync(root).map(readInfo));
 }
 
-async function readInfo(name) {
-  const filename = path.resolve(root, name);
-  const url = `/static/img/art/${name}`;
-  name = path
-    .basename(name)
+async function readInfo(filename) {
+  const url = `/static/img/art/${filename}`;
+  const cleanName = path
+    .basename(filename)
     .replace(/\.(png|gif)$/, "")
     .replace(/-/g, " ")
     .replace(/^(\d\d\d\d) (\d\d) (\d\d) (.*)$/, "$4 ($1-$2-$3)");
-  const { width, height } = getImageSize(filename);
-  const colors = await getImageColors(filename);
+  const [, name, date] = cleanName.match(/^(.*?) \((.*)\)$/);
+  const file = path.join(root, filename);
+  const { width, height } = getImageSize(file);
+  const colors = await getImageColors(file);
   const color = colors[0].hex();
-  return { url, name, width, height, color };
+  const obj = { url, name, date, width, height, color };
+  return obj;
 }
 
 // If this ever gets too slow, check out asset caching
