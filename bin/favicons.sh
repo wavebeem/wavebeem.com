58d73c6504ca616da31ea9b5421ace6180a63645
Brian Mock <brian@mockbrian.com>
2021-07-18 13:41:27 -0700

    cool

--- bin/favicons.sh
+++ bin/favicons.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env bash
 set -eu
 
+Resize() {
+  local src="$1"
+  local size="$2"
+  local dest="$3"
+  convert "$src" -scale "$size" -strip "$dest"
+}
+
 cd src/static
 
-convert \
-  "favicon-16.png" \
-  -scale "32x32" \
-  -strip \
-  "favicon-32.png"
+Resize "favicon-16.png" 32 "favicon-32.png"
 
 for size in 48 96 180 192 512; do
-  convert \
-    "brian-24.png" \
-    -scale "${size}x${size}" \
-    -strip \
-    "brian-${size}.png"
-
-  convert \
-    "brian-alpha-24.png" \
-    -scale "${size}x${size}" \
-    -strip \
-    "brian-alpha-${size}.png"
+  Resize "brian-24.png" $size "brian-${size}.png"
+  Resize "brian-alpha-24.png" $size "brian-alpha-${size}.png"
 done
