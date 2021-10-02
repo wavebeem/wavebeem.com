b583bfa943019a30d43369f96a0000f8fb3c375a
Brian Mock <brian@wavebeem.com>
2021-10-01 22:54:45 -0700

    okie dokie

--- bin/favicons.sh
+++ bin/favicons.sh
@@ -12,7 +12,6 @@ cd src/static
 
 Resize "favicon-16.png" 32 "favicon-32.png"
 
-for size in 48 96 180 192 512; do
+for size in 48 72 96 180 192 512; do
   Resize "brian-24.png" $size "brian-${size}.png"
-  Resize "brian-alpha-24.png" $size "brian-alpha-${size}.png"
 done
