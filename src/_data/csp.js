257763fc3b9600bd739a400770a535138dd05389
Brian Mock <brian@wavebeem.com>
2022-01-24 21:16:58 -0800

    Blog react hooks (#59)

--- src/_data/csp.js
+++ src/_data/csp.js
@@ -1,11 +1,22 @@
+const defaultSrc = ["'self'", "https://plausible.io"];
+const imgSrc = [...defaultSrc];
+const scriptSrc = [...defaultSrc];
+const styleSrc = ["'self'", "'unsafe-inline'"];
+
 // Allow 'unsafe-inline' JS sources for now, since Eleventy injects an inline
 // script tag that loads Browsersync, rather than injecting a regular script
 // tag.
-module.exports = `
-  default-src 'self' https://plausible.io;
-  img-src 'self' https://plausible.io;
-  script-src 'self' https://plausible.io ${
-    process.env.CI ? "" : "'unsafe-inline'"
-  };
-  style-src 'self' 'unsafe-inline';
-`;
+if (!process.env.CI) {
+  scriptSrc.push("'unsafe-inline'");
+}
+
+const csp = {
+  "default-src": defaultSrc,
+  "img-src": imgSrc,
+  "script-src": scriptSrc,
+  "style-src": styleSrc,
+};
+
+module.exports = Object.entries(csp)
+  .map(([k, v]) => `${k} ${v.join(" ")};`)
+  .join(" ");
