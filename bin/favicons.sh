7aa94a38a1565dfa2d63b6f075792dd491599973
Brian Mock <brian@wavebeem.com>
2022-04-27 11:30:35 -0700

    Add banner image for Twitter (#67)

--- bin/favicons.sh
+++ bin/favicons.sh
@@ -12,8 +12,6 @@ cd src/static
 
 Resize "favicon-16.png" 32 "favicon-32.png"
 
-for size in 48 96 180 192 512; do
-  Resize "brian-24.png" $size "brian-${size}.png"
+for size in 48 96 180 192 360 512; do
+  Resize "photo.jpg" $size "photo-${size}.jpg"
 done
-
-Resize "photo.jpg" 360 "photo-360.jpg"
