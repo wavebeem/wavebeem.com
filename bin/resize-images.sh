baf04f004f4f5dc16d4d0cad9b09a7e3a17fa1d0
Brian Mock <brian@wavebeem.com>
2022-08-31 22:29:33 -0700

    cool

--- bin/resize-images.sh
+++ bin/resize-images.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -eu
 
-Resize() {
+:resize() {
   local src="$1"
   local size="$2"
   local dest="$3"
-  magick convert "$src" -scale "$size" -strip "$dest"
+  magick "$src" -scale "$size" -strip "$dest"
 }
 
 cd src/static
 
-Resize "favicon-16.png" 32 "favicon-32.png"
+:resize "favicon-16.png" 32 "favicon-32.png"
 
 for size in 48 96 180 192 360 512; do
-  Resize "photo.jpg" $size "photo-${size}.jpg"
+  :resize "photo.jpg" "$size" "photo-${size}.jpg"
 done
