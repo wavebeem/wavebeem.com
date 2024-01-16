de2e19878b5ed6bad57ef21af7186979762d79a0
Brian Mock <brian@wavebeem.com>
2024-01-15 16:48:23 -0800

    use JPEGs not PNGs for photos...

--- bin/resize-images.sh
+++ bin/resize-images.sh
@@ -13,5 +13,5 @@ cd src/static
 :resize "favicon-16.png" 32 "favicon-32.png"
 
 for size in 48 96 180 192 360 512; do
-  :resize "photo.png" "$size" "photo-${size}.png"
+  :resize "photo.jpg" "$size" "photo-${size}.jpg"
 done
