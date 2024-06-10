4cb10338831d0690abb0bcae87062779113bbd74
Sage Fennel <sage@wavebeem.com>
2024-06-10 09:40:18 -0700

    better code colors

--- src/_data/colors.js
+++ src/_data/colors.js
@@ -33,15 +33,16 @@ module.exports = {
     "color-main-text1": oklch(0, 0, 0),
     "color-main-text2": oklch(30, 0, 0),
 
-    "color-syntax-comment": oklch(53, 80, 140),
-    "color-syntax-keyword": oklch(53, 80, 210),
-    "color-syntax-property": oklch(53, 80, 310),
-    "color-syntax-string": oklch(53, 80, 0),
-    "color-syntax-punctuation": oklch(50, 0, 0),
+    "color-syntax-comment": oklch(55, 0, 300),
+    "color-syntax-keyword": oklch(47, 100, 300),
+    "color-syntax-property": oklch(52, 100, 190),
+    "color-syntax-string": oklch(52, 100, 135),
+    "color-syntax-punctuation": oklch(55, 40, 300),
 
     "color-easter-egg-shadow": oklch(15, 0, 0, 40),
     "color-easter-egg-highlight": oklch(100, 0, 0, 50),
   },
+
   dark: {
     "color-main-accent-background1": oklch(80, 40, 290),
     "color-main-accent-background2": oklch(76, 40, 290),
@@ -58,11 +59,11 @@ module.exports = {
     "color-main-text1": oklch(100, 0, 0),
     "color-main-text2": oklch(85, 0, 0),
 
-    "color-syntax-comment": oklch(80, 40, 140),
-    "color-syntax-keyword": oklch(80, 40, 210),
-    "color-syntax-property": oklch(80, 40, 310),
-    "color-syntax-string": oklch(80, 40, 40),
-    "color-syntax-punctuation": oklch(70, 0, 0),
+    "color-syntax-comment": oklch(90, 30, 190),
+    "color-syntax-keyword": oklch(80, 70, 135),
+    "color-syntax-property": oklch(80, 70, 40),
+    "color-syntax-string": oklch(80, 70, 300),
+    "color-syntax-punctuation": oklch(70, 5, 190),
 
     "color-easter-egg-shadow": oklch(15, 0, 0, 100),
     "color-easter-egg-highlight": oklch(100, 0, 0, 30),
