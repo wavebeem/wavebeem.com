1de2a5a4f4cf9e72885e6c7e20599e0743d0997b
Sage Fennel <sage@wavebeem.com>
2024-07-26 11:21:09 -0700

    pls work

--- src/_data/origin.js
+++ src/_data/origin.js
@@ -4,7 +4,11 @@ const fallbackOrigin = process.env.CI
   ? "https://wavebeem.com"
   : "http://localhost:1312";
 
-const origin =
-  process.env.DEPLOY_PRIME_URL || process.env.URL || fallbackOrigin;
+const prettyOrigin =
+  process.env.CONTEXT === "production"
+    ? process.env.DEPLOY_PRIME_URL
+    : process.env.URL;
+
+const origin = prettyOrigin || fallbackOrigin;
 
 module.exports = origin;
