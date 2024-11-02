9913f1a97698246e74edf8838b5dc53d3ca8fbe5
Sage Fennel <sage@wavebeem.com>
2024-11-02 13:39:10 -0700

    more

--- astro/src/consts.ts
+++ astro/src/consts.ts
@@ -3,4 +3,5 @@
 
 export const SITE_TITLE = "Astro Blog";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_LANGUAGE = "en-US";
 export const MY_NAME = "Sage Fennel";
--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -3,19 +3,18 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      // Transform string to Date object
-      pubDate: z.coerce.date(),
-      tags: z
-        .array(z.string())
-        .optional()
-        .default(() => []),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional(),
-    }),
+  // schema: ({ image }) =>
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    tags: z
+      .array(z.string())
+      .optional()
+      .default(() => []),
+    // heroImage: image().optional(),
+  }),
 });
 
 export const collections = { blog };
