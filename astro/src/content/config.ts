563a10469adb1419828adc32d43183a7f6eec60e
Sage Fennel <sage@wavebeem.com>
2024-11-02 10:42:00 -0700

    more progress

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -3,15 +3,19 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    tags: z.array(z.string()).optional(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      tags: z
+        .array(z.string())
+        .optional()
+        .default(() => []),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
 });
 
 export const collections = { blog };
