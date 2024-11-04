79925cdd53a07e75e0cde6079419db6348a345f7
Sage Fennel <sage@wavebeem.com>
2024-11-03 23:31:01 -0800

    try to make my 11ty site better

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -18,6 +18,9 @@ export default function getConfig(config) {
 
   config.setLibrary("md", markdown);
   config.addPlugin(syntaxHighlight);
+  config.setLiquidOptions({
+    strictFilters: true,
+  });
   config.addPassthroughCopy({ "src/static": "/" });
   config.addPassthroughCopy("src/art/*.webp");
 
