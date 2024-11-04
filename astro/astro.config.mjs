55b5a060abd3eb874dfae2a81c601111d9e56f3e
Sage Fennel <sage@wavebeem.com>
2024-11-03 19:56:59 -0800

    ok ok

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -8,11 +8,21 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+  vite: {
+    build: {
+      // Don't use inline JS and stuff!
+      assetsInlineLimit: 0,
+    },
+  },
   markdown: {
     // Let's try Shiki later. My code already assumes Prism.
     syntaxHighlight: "prism",
   },
-  trailingSlash: "always",
+  // This causes "file endpoints" to also have trailing slashes :|
+  //
+  // https://github.com/withastro/astro/issues/9674
+  //
+  // trailingSlash: "always",
   site: "https://www.wavebeem.com",
   integrations: [mdx(), sitemap({})],
 });
--- astro/src/pages/feed.xml.ts
+++ astro/src/pages/feed.xml.ts
@@ -1,9 +1,9 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { getPosts } from "../data/posts";
 
-export async function GET(context: any) {
-  const posts = await getCollection("blog");
+export async function GET(context) {
+  const posts = await getPosts();
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
