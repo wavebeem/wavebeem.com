0b401163452d53ad30b06175c6a4b9291cbdf951
Sage Fennel <sage@wavebeem.com>
2024-12-01 11:05:16 -0800

    update the colors a bit more

--- src/_data/colors.mjs
+++ src/_data/colors.mjs
@@ -20,11 +20,11 @@ function oklch(lightness, chroma, hue, alpha = 100) {
 export const light = {
   "color-selection": oklch(90, 30, 140, 75),
 
-  "color-header-text": oklch(30, 40, 140),
+  "color-header-text": oklch(30, 0, 30),
   "color-header-background": oklch(85, 30, 140),
   "color-header-button": oklch(80, 30, 140),
 
-  "color-footer-text": oklch(45, 40, 290),
+  "color-footer-text": oklch(30, 0, 30),
   "color-footer-background": oklch(90, 40, 290),
 
   "color-card-background": oklch(94, 20, 30),
@@ -32,12 +32,12 @@ export const light = {
 
   "color-main-link": oklch(50, 50, 290),
   "color-main-background": oklch(98, 20, 30),
-  "color-main-text": oklch(40, 20, 30),
+  "color-main-text": oklch(30, 0, 30),
   "color-main-code": oklch(52, 100, 340),
   "color-main-decoration": oklch(60, 20, 30),
 
   "color-syntax-background": oklch(100, 20, 30),
-  "color-syntax-text": oklch(20, 30, 30),
+  "color-syntax-text": oklch(20, 0, 30),
   "color-syntax-comment": oklch(55, 0, 300),
   "color-syntax-keyword": oklch(47, 100, 170),
   "color-syntax-property": oklch(52, 100, 340),
@@ -51,11 +51,11 @@ export const light = {
 export const dark = {
   "color-selection": oklch(50, 20, 140, 75),
 
-  "color-header-text": oklch(96, 20, 140),
+  "color-header-text": oklch(95, 0, 30),
   "color-header-background": oklch(50, 20, 140),
   "color-header-button": oklch(45, 20, 140),
 
-  "color-footer-text": oklch(90, 40, 290),
+  "color-footer-text": oklch(95, 0, 30),
   "color-footer-background": oklch(35, 10, 290),
 
   "color-card-background": oklch(35, 4, 30),
@@ -63,12 +63,12 @@ export const dark = {
 
   "color-main-link": oklch(80, 100, 290),
   "color-main-background": oklch(30, 4, 30),
-  "color-main-text": oklch(85, 4, 30),
+  "color-main-text": oklch(95, 0, 30),
   "color-main-code": oklch(80, 40, 340),
   "color-main-decoration": oklch(60, 4, 30),
 
   "color-syntax-background": oklch(25, 4, 30),
-  "color-syntax-text": oklch(90, 5, 30),
+  "color-syntax-text": oklch(90, 0, 30),
   "color-syntax-comment": oklch(70, 10, 300),
   "color-syntax-keyword": oklch(80, 70, 170),
   "color-syntax-property": oklch(80, 70, 250),
