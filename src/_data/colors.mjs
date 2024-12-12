468fea113ba4e8fdd0839dc5492c6a5455ffc4f6
Sage Fennel <sage@wavebeem.com>
2024-12-12 00:00:17 -0800

    make it nice

--- src/_data/colors.mjs
+++ src/_data/colors.mjs
@@ -18,25 +18,25 @@ function oklch(lightness, chroma, hue, alpha = 100) {
 }
 
 export const light = {
-  "color-selection": oklch(90, 30, 140, 75),
+  "color-selection": oklch(64, 44, 149, 15),
 
-  "color-header-text": oklch(30, 0, 30),
-  "color-header-background": oklch(85, 30, 140),
-  "color-header-button": oklch(80, 30, 140),
+  "color-header-text": oklch(100, 0, 0),
+  "color-header-background": oklch(64, 44, 149),
+  "color-header-button": oklch(59, 44, 149),
 
-  "color-footer-text": oklch(30, 0, 30),
-  "color-footer-background": oklch(90, 40, 290),
+  "color-footer-text": oklch(30, 0, 0),
+  "color-footer-background": oklch(96, 5, 290),
 
-  "color-card-background": oklch(94, 20, 30),
-  "color-card-hover": oklch(96, 20, 30),
+  "color-card-background": oklch(96, 5, 290),
+  "color-card-hover": oklch(98, 5, 290),
 
   "color-main-link": oklch(50, 50, 290),
-  "color-main-background": oklch(98, 20, 30),
-  "color-main-text": oklch(30, 0, 30),
+  "color-main-background": oklch(100, 0, 0),
+  "color-main-text": oklch(30, 0, 0),
   "color-main-code": oklch(52, 100, 340),
-  "color-main-decoration": oklch(60, 20, 30),
+  "color-main-decoration": oklch(64, 44, 149),
 
-  "color-syntax-background": oklch(100, 20, 30),
+  "color-syntax-background": oklch(96, 5, 290),
   "color-syntax-text": oklch(20, 0, 30),
   "color-syntax-comment": oklch(55, 0, 300),
   "color-syntax-keyword": oklch(47, 100, 170),
@@ -44,28 +44,28 @@ export const light = {
   "color-syntax-string": oklch(52, 100, 250),
   "color-syntax-punctuation": oklch(55, 30, 30),
 
-  "color-easter-egg-shadow": oklch(90, 20, 30),
+  "color-easter-egg-shadow": oklch(90, 0, 0),
   "color-easter-egg-highlight": oklch(100, 0, 0, 70),
 };
 
 export const dark = {
-  "color-selection": oklch(50, 20, 140, 75),
+  "color-selection": oklch(75, 44, 149, 25),
 
-  "color-header-text": oklch(95, 0, 30),
-  "color-header-background": oklch(50, 20, 140),
-  "color-header-button": oklch(45, 20, 140),
+  "color-header-text": oklch(100, 0, 0),
+  "color-header-background": oklch(64, 44, 149),
+  "color-header-button": oklch(59, 44, 149),
 
   "color-footer-text": oklch(95, 0, 30),
-  "color-footer-background": oklch(35, 10, 290),
+  "color-footer-background": oklch(35, 5, 290),
 
-  "color-card-background": oklch(35, 4, 30),
-  "color-card-hover": oklch(37, 4, 30),
+  "color-card-background": oklch(35, 5, 290),
+  "color-card-hover": oklch(37, 5, 290),
 
   "color-main-link": oklch(80, 100, 290),
-  "color-main-background": oklch(30, 4, 30),
+  "color-main-background": oklch(30, 5, 290),
   "color-main-text": oklch(95, 0, 30),
   "color-main-code": oklch(80, 40, 340),
-  "color-main-decoration": oklch(60, 4, 30),
+  "color-main-decoration": oklch(75, 36, 152),
 
   "color-syntax-background": oklch(25, 4, 30),
   "color-syntax-text": oklch(90, 0, 30),
