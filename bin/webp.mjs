58c63c934201c64018c42be5738f9e8df1f27165
Sage Fennel <sage@wavebeem.com>
2025-01-13 00:05:37 -0800

    idk

---
+++ bin/webp.mjs
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import { spawnSync } from "node:child_process";
+import { chdir } from "node:process";
+import { Glob } from "glob";
+import { existsSync } from "node:fs";
+
+async function main([flag]) {
+  chdir("src/static");
+
+  const globDirs = ["art", "blog", "shrines", "design-history"];
+  const globRoots = "{" + globDirs.join(",") + "}";
+
+  for await (const file of new Glob(`${globRoots}/**/*.{png,gif}`, {})) {
+    for (const [, base] of match(file, /^(.*)\.(png|gif)/)) {
+      const dest = `${base}.webp`;
+      if (flag === "force" || !existsSync(dest)) {
+        console.log(file);
+        spawnSync("magick", [file, "-quality", "100", dest]);
+      }
+    }
+  }
+
+  for await (const file of new Glob(`${globRoots}/**/*.{jpg,jpeg}`, {})) {
+    for (const [, base] of match(file, /^(.*)\.(jpg|jpeg)/)) {
+      const dest = `${base}.webp`;
+      if (flag === "force" || !existsSync(dest)) {
+        console.log(file);
+        spawnSync("magick", [file, "-resize", "800>", dest]);
+      }
+    }
+  }
+}
+
+function* match(string, regexp) {
+  const m = string.match(regexp);
+  if (m) {
+    yield m;
+  }
+}
+
+main(process.argv.slice(2));
