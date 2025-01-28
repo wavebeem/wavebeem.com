42fcfa41d0687641b977b17ba052f1167b78247a
Sage Fennel <sage@wavebeem.com>
2025-01-28 15:07:14 -0800

    smallen the image lol

---
+++ bin/photo.mjs
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { spawnSync } from "node:child_process";
+import { chdir } from "node:process";
+
+async function main([flag]) {
+  chdir("src");
+  for (const size of [180, 360]) {
+    spawnSync("magick", ["photo.webp", "-resize", size, `photo-${size}.webp`]);
+  }
+}
+
+main(process.argv.slice(2));
