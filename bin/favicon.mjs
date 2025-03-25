3aa0ff3d63fcc248da432d45d44b14be4c3d41f8
Sage Fennel <sage@wavebeem.com>
2025-03-24 17:44:37 -0700

    icon

--- bin/favicon.mjs
+++ bin/favicon.mjs
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 import { run, chdir } from "./_util.mjs";
 
-async function main([flag]) {
-  chdir("src/static");
-  await run("magick", "favicon.png", "favicon.ico");
-}
-
-main(process.argv.slice(2));
+chdir("src/static");
+await run("magick", "favicon.png", "favicon.ico");
--- bin/photo.mjs
+++ bin/photo.mjs
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 import { run, chdir } from "./_util.mjs";
 
-async function main([flag]) {
-  chdir("src");
-  for (const size of [180, 360]) {
-    await run("magick", "photo.webp", "-resize", size, `photo-${size}.webp`);
-  }
+chdir("src");
+for (const size of [180, 360]) {
+  await run("magick", "photo.webp", "-resize", size, `photo-${size}.webp`);
 }
-
-main(process.argv.slice(2));
