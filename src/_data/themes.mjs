d537d5b0a02ca5651a845ded673e52c1ff656654
Sage Fennel <sage@wavebeem.com>
2025-04-23 23:02:59 -0700

    ok

--- src/_data/themes.mjs
+++ src/_data/themes.mjs
@@ -22,13 +22,13 @@ export const light = {
   "color-root-background-alpha": "#3333",
   "color-root-overscroll": "#222",
   "color-root-gradient1": oklch(55, 55, 152),
-  "color-root-gradient2": oklch(55, 55, 290),
+  "color-root-gradient2": oklch(55, 50, 290),
+  "color-root-gradient3": oklch(45, 35, 290),
   "color-root-text": "#fff",
   "color-root-divider": "#fff3",
 
   "color-main-background": "#eee",
   "color-main-text": "#333",
-  "color-main-text-small": "#666",
   "color-main-divider": "#ccc",
   "color-main-link": oklch(50, 65, 290),
 
@@ -44,7 +44,6 @@ export const light = {
 export const dark = {
   "color-main-background": "#333",
   "color-main-text": "#eee",
-  "color-main-text-small": "#aaa",
   "color-main-divider": "#555",
   "color-main-link": oklch(80, 65, 290),
 
