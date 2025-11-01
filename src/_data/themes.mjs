19a439ba0357e4ef51d90220d10ca6fc4016de79
Sage Fennel <sage@wavebeem.com>
2025-11-01 12:08:37 -0700

    syntax highlighting

--- src/_data/themes.mjs
+++ src/_data/themes.mjs
@@ -22,16 +22,19 @@ export default {
 
   "color-green": oklch(74, 38, 162),
 
+  "color-pink": oklch(71, 50, 345),
+
   "color-purple": oklch(47, 67, 288),
   "color-periwinkle": oklch(85, 67, 288),
 
+  "color-steel": oklch(91, 1.75, 89),
   "color-silver": oklch(95, 1.75, 89),
   "color-white": oklch(99.5, 1.75, 89),
 
   "syntax-text": "var(--color-black)",
-  "syntax-comment": oklch(52, 30, 40),
-  "syntax-keyword": oklch(50, 38, 162),
-  "syntax-property": oklch(47, 67, 299),
-  "syntax-string": oklch(55, 67, 89),
+  "syntax-keyword": oklch(52, 60, 260),
+  "syntax-comment": oklch(50, 38, 162),
+  "syntax-property": oklch(47, 67, 345),
+  "syntax-string": oklch(55, 67, 42),
   "syntax-punctuation": oklch(57, 27, 288),
 };
