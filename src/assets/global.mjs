04bd8e6850a7f8b93ae96f2445b244a070282c5a
Sage Fennel <sage@wavebeem.com>
2025-11-03 19:52:28 -0800

    better

--- src/assets/global.mjs
+++ src/assets/global.mjs
@@ -1,5 +1,50 @@
-const url = new URL(location.href);
-const theme = url.searchParams.get("theme");
-if (theme) {
-  document.documentElement.dataset.theme = theme;
+const root = document.documentElement;
+
+// Apply theme parameters to the document
+{
+  const url = new URL(location.href);
+  const theme = url.searchParams.get("theme");
+  if (theme) {
+    document.documentElement.dataset.theme = theme;
+  }
+}
+
+// Add date information to document
+{
+  const weekdays = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+  ];
+  const date = new Date();
+  root.dataset.year = date.getFullYear();
+  root.dataset.month = date.getMonth() + 1;
+  root.dataset.date = date.getDate();
+  root.dataset.day = weekdays[date.getDay()];
+}
+
+// Enable JS figure image viewer
+{
+  root.dataset.figureViewer = "";
+  addEventListener("click", (event) => {
+    if (!(event.target instanceof Element)) {
+      return;
+    }
+    const img = event.target.closest("img");
+    const figure = img.closest("figure");
+    if (!(img && figure)) {
+      return;
+    }
+    const wantsNewTab =
+      event.shiftKey || event.metaKey || event.ctrlKey || event.button === 1;
+    event.preventDefault();
+    if (wantsNewTab) {
+      window.open(img.src, "_blank");
+    } else {
+      location.href = img.src;
+    }
+  });
 }
