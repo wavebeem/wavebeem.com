38222cc518cf132765002bf52ec1afe7c8ffd400
Sage Fennel <sage@wavebeem.com>
2026-01-08 00:09:15 -0800

    colors

--- src/_data/themes.mjs
+++ src/_data/themes.mjs
@@ -17,26 +17,33 @@ function oklch(lightness, chroma, hue, alpha = 100) {
   return color.to("srgb").toString({ format: "hex" });
 }
 
+// https://www.coderstool.com/split-complementary-color-scheme
+// BLURPLE  #6103e3
+// PINK     #e30358
+// GREEN    #03e307
+
 export default {
-  "color-black": oklch(27, 3.25, 299),
+  "color-black": oklch(27, 3.25, 11),
 
-  "color-border-faint": oklch(27, 3.25, 299, 10),
-  "color-border-light": oklch(27, 3.25, 299, 20),
-  "color-border-medium": oklch(27, 3.25, 299, 40),
-  "color-border-heavy": oklch(27, 3.25, 299, 60),
+  "color-border-faint": oklch(27, 3.25, 11, 10),
+  "color-border-light": oklch(27, 3.25, 11, 20),
+  "color-border-medium": oklch(27, 3.25, 11, 40),
+  "color-border-heavy": oklch(27, 3.25, 11, 60),
 
-  "color-green": oklch(74, 38, 162),
+  // TODO: Use the pink anywhere?
+  // TODO: Use the green more?
 
-  "color-pink": oklch(71, 50, 345),
+  "color-green": oklch(80, 50, 142),
 
   "color-purple": oklch(47, 67, 288),
   "color-periwinkle": oklch(85, 67, 288),
 
-  "color-steel": oklch(92, 0.5, 68),
-  "color-silver": oklch(94, 0.5, 68),
-  "color-white": oklch(99.5, 0.5, 68),
+  "color-steel": oklch(92, 0.5, 11),
+  "color-silver": oklch(94, 0.5, 11),
+  "color-white": oklch(99.5, 0.5, 11),
 
   "syntax-text": "var(--color-black)",
+  // TODO: Improve these
   "syntax-keyword": oklch(52, 60, 260),
   "syntax-comment": oklch(50, 38, 162),
   "syntax-property": oklch(47, 67, 345),
