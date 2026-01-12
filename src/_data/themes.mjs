1a7e31be706b94c329be2cb284c3ba5359d2e856
Sage Fennel <sage@wavebeem.com>
2026-01-12 10:40:28 -0800

    yea

--- src/_data/themes.mjs
+++ src/_data/themes.mjs
@@ -26,11 +26,11 @@ export default {
   "color-green": oklch(80, 40, 142),
   "color-greener": oklch(60, 50, 142),
 
-  "color-steel": oklch(92, 0.5, 322),
   "color-silver": oklch(98, 0.5, 322),
   "color-offwhite": oklch(98, 0.5, 322),
   "color-white": oklch(99.5, 0.5, 322),
 
+  "theme-button": oklch(90, 0.5, 322),
   "theme-photo": oklch(96, 0.5, 322),
   "theme-card": oklch(96, 0.5, 322),
   // "theme-card": oklch(96, 0.5, 322),
@@ -38,7 +38,6 @@ export default {
   "theme-grid-dark": oklch(96, 0.5, 322),
   "theme-grid": oklch(96, 0.5, 322),
   "theme-link": oklch(50, 50, 142),
-  "theme-button": oklch(92, 0.5, 322),
 
   "syntax-text": "var(--color-black)",
   // TODO: Improve these
