620b09a80cdfe6c04d3639b070837775e0355069
Sage Fennel Mock <sage@wavebeem.com>
2026-09-12 12:25:44 -0700

    remove unused files

--- public/theme-init.js
+++
@@ -1,9 +0,0 @@
-{
-  const stored = localStorage.getItem("theme") || "auto";
-  const darkModeQuery = matchMedia("(prefers-color-scheme: dark)");
-  let theme = stored;
-  if (theme === "auto") {
-    theme = darkModeQuery.matches ? "dark" : "light";
-  }
-  document.documentElement.dataset.theme = theme;
-}
